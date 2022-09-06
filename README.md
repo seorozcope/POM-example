2555de596556b9f43403774dd5126c6b05eb09d1
seorozcope <seorozcope@gmail.com>
2022-09-06 17:04:28 -0500

    Added gradle command to run tests

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # POM-example
 
-POM example project using Gradle and TestNG
+In this project we will see how to implement an automation testing strategy based on UI elements using Selenium, Cucumber, and TestNG.
+_____
+
+## Prerequisites:
+
+* Gradle min version 7.5.1
+* JDK 16
+
+_____
+
+## Build the project
+
+At root folder, run the following command to build the dependencies:
+
+Windows
+```
+gradlew build
+```
+
+Unix based OS (e.g. MacOS, Linux)
+```
+./gradlew build
+```
+
+_____
+
+## Run the test suit
+
+At root folder run the following command to run the tests:
+
+Windows
+```
+gradlew test
+```
+
+Unix based OS (e.g. MacOS, Linux)
+```
+./gradlew test
+```
+
--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -3,8 +3,10 @@ package stepdefinitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -19,8 +21,17 @@ public class Hooks {
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox")
+                .addArguments("--disable-download-notification")
+                .addArguments("--headless")
+                .addArguments("disable-gpu")
+                .addArguments("--ignore-certificate-error")
+                .addArguments("--always-authorize-plugins")
+                .addArguments("--disable-extensions")
+                .addArguments("--window-size=1920,1080")
+                .addArguments("--disable-popup-blocking");
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
